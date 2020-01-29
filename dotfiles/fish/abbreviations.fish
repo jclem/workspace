d2cefad299b7f4ebca78a0fedd044dc593c6f4fc
Jonathan Clem <jonathan@jclem.net>
2020-01-29 11:36:17 -0500

    Add `tf` (terraform) to abbreviations

--- dotfiles/fish/abbreviations.fish
+++ dotfiles/fish/abbreviations.fish
@@ -8,5 +8,6 @@ if status --is-interactive
     abbr --add --global g git
     abbr --add --global hk heroku
     abbr --add --global n npm
+    abbr --add --global tf terraform
     abbr --add --global y yarn
 end
